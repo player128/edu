f51ddc758e16c6dc0139f9387326648e5128c03e
Slava <slavamonster6@gmail.com>
2023-07-06 19:53:45 +0300

    'begin'

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+    </head>
+</html>
