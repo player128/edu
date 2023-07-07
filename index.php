2678743c41c4342a9fe390de688617bc10299976
Slava <slavamonster6@gmail.com>
2023-07-07 18:08:57 +0300

    add main page

--- index.php
+++ index.php
@@ -1,7 +1,21 @@
 <html>
     <head>
         <meta charset='utf-8'>
-        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css\style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
     </head>
+    <body>
+        <div class="content content-center border">
+            <div class="elem border"></div>
+            <div class="elem border"></div>
+            <div class="elem border"></div>
+            <div class="elem border"></div>
+            <div class="elem border"></div>
+            <div class="elem border"></div>
+            <div class="elem border"></div>
+            <div class="elem border"></div>
+            <div class="elem border"></div>
+            <div class="elem border"></div>
+        </div>
+    </body>
 </html>
