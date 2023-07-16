1c4c4a85e4f68634045f9ae598d09711b11e51e3
Slava <slavamonster6@gmail.com>
2023-07-16 20:14:50 +0300

    add login

--- app/views/template_view.php
+++ app/views/template_view.php
@@ -4,9 +4,18 @@
         <link rel="stylesheet" href="http://localhost/edu/css/norm.css">
         <link rel="stylesheet" href="http://localhost/edu/css/style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+        <script defer src="http://localhost/edu/scripts/user.js"></script>
     </head>
     <body>
+        <div class="top">
+            <div class="user">
+                
+            </div>
+        </div>
         <div class="content content-center">
+        <?php 
+
+        ?>
         <?php include 'app/views/'.$content_view; ?>
         </div>
     </body>
